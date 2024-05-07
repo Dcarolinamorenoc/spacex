99dd340ca3c7ad5dbaebb6b62b38f5af93661466
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-05-07 10:37:13 -0500

    feat: :bug: Cambio de icono en el footer
    Se implemento un cambio de icono para el apartado de capsules

--- js/modulesComponents/title.js
+++ js/modulesComponents/title.js
@@ -2,4 +2,6 @@ export const nameRockets = async(name)=>{
     let header__title = document.querySelector("#header__title");
     header__title.innerHTML = "";
     header__title.textContent = name;
+    // cambio de letra se hace con:
+    // header__title.style.fontFamily = "Brush Script MT";
 }
