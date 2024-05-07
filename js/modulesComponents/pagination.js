ca82ac67434463b68460fcaf177e7b215520f27d
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-05-07 11:36:30 -0500

    feat: :construction: Se implemento el apartado de Capsules
    Voy a empezar a trabajar en el apartado capsules

--- js/modulesComponents/pagination.js
+++ js/modulesComponents/pagination.js
@@ -30,27 +30,94 @@ import {
     progressSecondStageDiameterRocket,
     progressSecondStageHeightRocket,
 } from "../modulesComponents/progressBar.js";
+///
+import { 
+    getAllCapsules 
+} from "../modules/capsules.js";
 
 
+export const load = async()=>{
+    let header__title = document.querySelector("#header__title");
+    header__title.innerHTML = `
+        <div class="load"></div>
+    `;
+
+    let description__item = document.querySelector("#description__item");
+    description__item.innerHTML = `
+        <div class="load"></div>
+        <div class="load"></div>
+        <div class="load"></div>
+        <div class="load"></div>
+        <div class="load"></div>
+    `;
+
+    let section__information__1 = document.querySelector("#section__information__1")
+    section__information__1.innerHTML = `
+        <div class="load" style="height: 150px;"></div>
+    `;
+
+    let information__table__1 = document.querySelector("#information__table__1")
+    information__table__1.innerHTML = `
+        <div class="load" style="height: 160px;"></div>
+    `;
+
+    let section__image = document.querySelector("#section__image")
+    section__image.innerHTML = `
+        <div class="load" style="height: 350px"></div>
+    `;
+
+
+    let information__table__2 = document.querySelector("#information__table__2")
+    information__table__2.innerHTML = `
+        <div class="load" style="height: 160px;"></div>
+    `;
+
+    let information__2 = document.querySelector("#information__2")
+    information__2.innerHTML = `
+        <div class="load"></div>
+        <div class="load"></div>
+        <div class="load"></div>
+        <div class="load"></div>
+        <div class="load"></div>
+    `;
+}
+export const clear = async()=>{
+    let header__title = document.querySelector("#header__title");
+    header__title.innerHTML = ``;
+
+    let description__item = document.querySelector("#description__item");
+    description__item.innerHTML = ``;
+
+    let section__information__1 = document.querySelector("#section__information__1")
+    section__information__1.innerHTML = ``;
+
+    let information__table__1 = document.querySelector("#information__table__1")
+    information__table__1.innerHTML = ``;
+
+    let section__image = document.querySelector("#section__image")
+    section__image.innerHTML = ``;
+
+
+    let information__table__2 = document.querySelector("#information__table__2")
+    information__table__2.innerHTML = ``;
+
+    let information__2 = document.querySelector("#information__2")
+    information__2.innerHTML = ``;
+
+}
 
 const getRocketsId = async(e)=>{
     e.preventDefault();
+    // console.log(e.target);
     let a = e.target.parentElement.children;
     for(let val of a){
         val.classList.remove('activo');
     }
     e.target.classList.add('activo');
     
-    let information__2 = document.querySelector("#information__2");
-    information__2.innerHTML = "";
-    let description__item = document.querySelector("#description__item")
-    description__item.innerHTML = "";
-    let section__image = document.querySelector("#section__image")
-    section__image.innerHTML = "";
-
     let Rocket = await getAllRocketsId(e.target.id);
-    console.log(Rocket);
-
+    await clear();
+    
     await informationRockets(Rocket.country, Rocket.description)
     await nameRockets(Rocket.name)
     await informationLaunchCostRocket(Rocket.cost_per_launch)
@@ -105,36 +172,66 @@ export const paginationRockets = async () => {
     return div;
 }
 
-// export const paginationRockets = async () => {
-//     let rockets = await getAllRockets();
-//     let div = document.createElement("div");
-//     div.classList.add("button__pagination");
+const getCapsulesId = async(e)=>{
+    e.preventDefault();
+    if(e.target.dataset.page){
+        let paginacion = document.querySelector("#paginacion");
+        paginacion.innerHTML = ""
+        paginacion.append(await paginationCapsules(Number(e.target.dataset.page)))
+    }
+    let a = e.target.parentElement.children;
+    for(let val of a){
+        val.classList.remove('activo');
+    }
+    e.target.classList.add('activo');
+    
 
-//     rockets.forEach((val, id) => {
-//         let a = document.createElement("a");
-//         a.setAttribute("href", "#");
-//         a.id = val.id;
-//         a.textContent = id + 1;
-//         a.addEventListener("click", getRocketsId);
-//         a.addEventListener("click", function() {
-//             // Remover todas las clases de gradiente del cuerpo
-//             document.body.classList.remove("gradient1", "gradient2", "gradient3", "gradient4");
-//             // Agregar la clase correspondiente al gradiente según el índice
-//             if (id === 0) {
-//                 document.body.classList.add("gradient1");
-//             } else if (id === 1) {
-//                 document.body.classList.add("gradient2");
-//             } else if (id === 2) {
-//                 document.body.classList.add("gradient3");
-//             } else if (id === 3) {
-//                 document.body.classList.add("gradient4");
-//             }
-//         });
-//         div.appendChild(a);
-//     });
+    // let Rocket = await getAllRocketsId(e.target.id);
+    // console.log(Rocket);
 
-//     let [a1, a2, a3, a4] = div.children;
-//     a3.click(); // Hacer clic en el tercer enlace por defecto
+    // await informationRockets(Rocket.country, Rocket.description)
+    
+}
 
-//     return div;
-// }
+export const paginationCapsules = async(page=1, limit=4)=>{  
+     
+    let {docs, pagingCounter, totalPages, nextPage} = await getAllCapsules(page, limit)
+
+    let div = document.createElement("div");
+    div.classList.add("buttom__paginacion")
+
+    
+    let start = document.createElement("a");
+    start.setAttribute("href","#");
+    start.innerHTML = "&laquo";
+    start.setAttribute("data-page", (page==1) ? totalPages : page-1)
+    start.addEventListener("click", getCapsulesId)
+    div.appendChild(start);
+    docs.forEach((val,id) => {
+        let a = document.createElement("a");
+        a.setAttribute("href","#");
+        a.id = val.id;
+        a.textContent = pagingCounter;
+        a.addEventListener("click", getCapsulesId)
+        div.appendChild(a);
+        pagingCounter++
+    });
+    let end = document.createElement("a");
+    end.setAttribute("href","#");
+    end.innerHTML = "&raquo;";
+    end.setAttribute("data-page", (page && nextPage) ? page+1 : 1)
+    end.addEventListener("click", getCapsulesId)
+    div.appendChild(end);
+    console.log(div);
+    let [back, a1,a2,a3,a4, next] = div.children
+    a1.click();
+    // <div class="buttom__paginacion">
+    //     <a href="#">&laquo;</a> 
+    //     <a href="#" class="activo">1</a>
+    //     <a href="#">2</a>
+    //     <a href="#">3</a>
+    //     <a href="#">4</a>
+    //     <a href="#">&raquo;</a>
+    // </div>
+    return div;
+}
