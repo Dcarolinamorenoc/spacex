549c37c006b5de04611e68f0f216155fca3b3e4e
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-05-10 00:01:17 -0500

    feat: :lipstick: Se le cambio el color de fondo a las barras de progreso
    de tal manera que para observar los datos fuera facil se les aplico un color

--- js/modulesComponents/progressBar.js
+++ js/modulesComponents/progressBar.js
@@ -6,129 +6,110 @@ import {
     getRocketSecondStageCompositeFairingDiameterTotal,
     getRocketSecondStageCompositeFairingHeightTotal
 } from "../modules/rockets.js";
-export const progressRocketWeight = async(Rockets)=>{
-    let {kg} = await getRocketMassTotal();
+
+
+export const progressRocketWeight = async (Rockets) => {
+    let { kg } = await getRocketMassTotal();
     let conterDiv = [];
     [Rockets].forEach((val) => {
         let divInformationContainer = document.createElement("div");
-        divInformationContainer.classList.add("information__container")
+        divInformationContainer.classList.add("information__container");
         let divFirst = document.createElement("div");
         let labelFist = document.createElement("label");
         labelFist.textContent = "Rocket weight :";
-    
+
         let ProgressFist = document.createElement("progress");
-        ProgressFist.max = kg
+        ProgressFist.max = kg;
         ProgressFist.value = `${val.mass.kg}`;
         ProgressFist.textContent = `${val.mass.kg}%`;
-    
-    
+        ProgressFist.style.backgroundColor = 'black'; // Cambiar color de la barra a negro
+
         let divLast = document.createElement("div");
         let spanLast = document.createElement("span");
-        let numKg = new Intl.NumberFormat('cop').format(val.mass.kg)
-        let numLb = new Intl.NumberFormat('cop').format(val.mass.lb)
-        spanLast.innerHTML = `${numKg} kg <br> ${numLb} lb`
-    
-        divFirst.append(labelFist)
-        divFirst.append(ProgressFist)
-        divLast.append(spanLast)
-        divInformationContainer.append(divFirst)
-        divInformationContainer.append(divLast)
-        conterDiv.push(divInformationContainer)
+        let numKg = new Intl.NumberFormat('cop').format(val.mass.kg);
+        let numLb = new Intl.NumberFormat('cop').format(val.mass.lb);
+        spanLast.innerHTML = `${numKg} kg <br> ${numLb} lb`;
+
+        divFirst.append(labelFist);
+        divFirst.append(ProgressFist);
+        divLast.append(spanLast);
+        divInformationContainer.append(divFirst);
+        divInformationContainer.append(divLast);
+        conterDiv.push(divInformationContainer);
     });
     let information__2 = document.querySelector("#information__2");
-    information__2.append(...conterDiv)
-    // <div class="information__container">
-    //     <div>
-    //         <label>Totle :</label>
-    //         <progress max="100" value="70">70%</progress>
-    //     </div>
-    //     <div>
-    //         <span>0 kg <br> 0 lb</span>
-    //     </div>
-    // </div>
+    information__2.append(...conterDiv);
 }
-export const progressPayloadWeights = async(Rockets)=>{
-    let {kg} = await getRocketPayloadWeightsTotal();
+
+
+export const progressPayloadWeights = async (Rockets) => {
+    let { kg } = await getRocketPayloadWeightsTotal();
     let conterDiv = [];
     Rockets.payload_weights.forEach(val => {
         let divInformationContainer = document.createElement("div");
-        divInformationContainer.classList.add("information__container")
+        divInformationContainer.classList.add("information__container");
         let divFirst = document.createElement("div");
         let labelFist = document.createElement("label");
         labelFist.textContent = `${val.name} :`;
-    
+
         let ProgressFist = document.createElement("progress");
         ProgressFist.max = kg;
         ProgressFist.value = `${val.kg}`;
         ProgressFist.textContent = `${val.kg}%`;
-    
-    
+        ProgressFist.style.backgroundColor = 'black'; // Cambiar color de la barra a negro
+
         let divLast = document.createElement("div");
         let spanLast = document.createElement("span");
-        let numKg = new Intl.NumberFormat('cop').format(val.kg)
-        let numLb = new Intl.NumberFormat('cop').format(val.lb)
-        spanLast.innerHTML = `${numKg} kg <br> ${numLb} lb`
-    
-        divFirst.append(labelFist)
-        divFirst.append(ProgressFist)
-        divLast.append(spanLast)
-        divInformationContainer.append(divFirst)
-        divInformationContainer.append(divLast)
-        conterDiv.push(divInformationContainer)
+        let numKg = new Intl.NumberFormat('cop').format(val.kg);
+        let numLb = new Intl.NumberFormat('cop').format(val.lb);
+        spanLast.innerHTML = `${numKg} kg <br> ${numLb} lb`;
+
+        divFirst.append(labelFist);
+        divFirst.append(ProgressFist);
+        divLast.append(spanLast);
+        divInformationContainer.append(divFirst);
+        divInformationContainer.append(divLast);
+        conterDiv.push(divInformationContainer);
     });
     let information__2 = document.querySelector("#information__2");
-    information__2.append(...conterDiv)
-    // <div class="information__container">
-    //     <div>
-    //         <label>Totle :</label>
-    //         <progress max="100" value="70">70%</progress>
-    //     </div>
-    //     <div>
-    //         <span>0 kg <br> 0 lb</span>
-    //     </div>
-    // </div>
+    information__2.append(...conterDiv);
 }
-export const progressHeightRocket = async(Rockets)=>{
-    let {meters} = await getRocketHeightTotal();
+
+export const progressHeightRocket = async (Rockets) => {
+    let { meters } = await getRocketHeightTotal();
     let conterDiv = [];
     [Rockets.height].forEach(val => {
         let divInformationContainer = document.createElement("div");
-        divInformationContainer.classList.add("information__container")
+        divInformationContainer.classList.add("information__container");
         let divFirst = document.createElement("div");
         let labelFist = document.createElement("label");
         labelFist.textContent = `Rocket Height :`;
-    
+
         let ProgressFist = document.createElement("progress");
         ProgressFist.max = meters;
         ProgressFist.value = `${val.meters}`;
         ProgressFist.textContent = `${val.meters}%`;
-    
-    
+        ProgressFist.style.backgroundColor = 'black'; // Cambia el color de la barra de progreso a negro
+
         let divLast = document.createElement("div");
         let spanLast = document.createElement("span");
-        let numMeters = new Intl.NumberFormat('cop').format(val.meters)
-        let numFeet = new Intl.NumberFormat('cop').format(val.feet)
-        spanLast.innerHTML = `${numMeters} M <br> ${numFeet} F`
-    
-        divFirst.append(labelFist)
-        divFirst.append(ProgressFist)
-        divLast.append(spanLast)
-        divInformationContainer.append(divFirst)
-        divInformationContainer.append(divLast)
-        conterDiv.push(divInformationContainer)
+        let numMeters = new Intl.NumberFormat('cop').format(val.meters);
+        let numFeet = new Intl.NumberFormat('cop').format(val.feet);
+        spanLast.innerHTML = `${numMeters} M <br> ${numFeet} F`;
+
+        divFirst.append(labelFist);
+        divFirst.append(ProgressFist);
+        divLast.append(spanLast);
+        divInformationContainer.append(divFirst);
+        divInformationContainer.append(divLast);
+        conterDiv.push(divInformationContainer);
     });
     let information__2 = document.querySelector("#information__2");
-    information__2.append(...conterDiv)
-    // <div class="information__container">
-    //     <div>
-    //         <label>Totle :</label>
-    //         <progress max="100" value="70">70%</progress>
-    //     </div>
-    //     <div>
-    //         <span>0 kg <br> 0 lb</span>
-    //     </div>
-    // </div>
+    information__2.append(...conterDiv);
 }
+
+
+
 export const progressDiameterRocket = async(Rockets)=>{
     let {meters} = await getRocketDiameterTotal();
     let conterDiv = [];
@@ -143,7 +124,7 @@ export const progressDiameterRocket = async(Rockets)=>{
         ProgressFist.max = meters;
         ProgressFist.value = `${val.meters}`;
         ProgressFist.textContent = `${val.meters}%`;
-    
+        ProgressFist.style.setProperty('background-color', 'black'); // Cambiar color de la barra
     
         let divLast = document.createElement("div");
         let spanLast = document.createElement("span");
@@ -160,16 +141,9 @@ export const progressDiameterRocket = async(Rockets)=>{
     });
     let information__2 = document.querySelector("#information__2");
     information__2.append(...conterDiv)
-    // <div class="information__container">
-    //     <div>
-    //         <label>Totle :</label>
-    //         <progress max="100" value="70">70%</progress>
-    //     </div>
-    //     <div>
-    //         <span>0 kg <br> 0 lb</span>
-    //     </div>
-    // </div>
 }
+
+
 export const progressSecondStageDiameterRocket = async(Rockets)=>{
     let {meters} = await getRocketSecondStageCompositeFairingDiameterTotal();
     let conterDiv = [];
@@ -184,7 +158,7 @@ export const progressSecondStageDiameterRocket = async(Rockets)=>{
         ProgressFist.max = meters;
         ProgressFist.value = `${val.meters}`;
         ProgressFist.textContent = `${val.meters}%`;
-    
+        ProgressFist.style.setProperty('background-color', 'black'); // Cambiar color de la barra
     
         let divLast = document.createElement("div");
         let spanLast = document.createElement("span");
@@ -201,16 +175,9 @@ export const progressSecondStageDiameterRocket = async(Rockets)=>{
     });
     let information__2 = document.querySelector("#information__2");
     information__2.append(...conterDiv)
-    // <div class="information__container">
-    //     <div>
-    //         <label>Totle :</label>
-    //         <progress max="100" value="70">70%</progress>
-    //     </div>
-    //     <div>
-    //         <span>0 kg <br> 0 lb</span>
-    //     </div>
-    // </div>
 }
+
+
 export const progressSecondStageHeightRocket = async(Rockets)=>{
     let {meters} = await getRocketSecondStageCompositeFairingHeightTotal();
     let conterDiv = [];
@@ -225,7 +192,7 @@ export const progressSecondStageHeightRocket = async(Rockets)=>{
         ProgressFist.max = meters;
         ProgressFist.value = `${val.meters}`;
         ProgressFist.textContent = `${val.meters}%`;
-    
+        ProgressFist.style.setProperty('background-color', 'black'); // Cambiar color de la barra
     
         let divLast = document.createElement("div");
         let spanLast = document.createElement("span");
@@ -242,13 +209,4 @@ export const progressSecondStageHeightRocket = async(Rockets)=>{
     });
     let information__2 = document.querySelector("#information__2");
     information__2.append(...conterDiv)
-    // <div class="information__container">
-    //     <div>
-    //         <label>Totle :</label>
-    //         <progress max="100" value="70">70%</progress>
-    //     </div>
-    //     <div>
-    //         <span>0 kg <br> 0 lb</span>
-    //     </div>
-    // </div>
 }
